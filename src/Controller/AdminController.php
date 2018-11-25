21852cf377a2c9027d6aa782722e3aa1f19969a9
Luděk Veselý <ludek.vesely@email.com>
2018-11-25 23:41:36 +0100

    create product administration

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/admin")
+ */
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/", name="admin_index")
+     */
+    public function index(Request $request): Response
+    {
+        return $this->render('admin/index.html.twig');
+    }
+}
