9ab45ef5a14740706ff6d133ebad07672b1d7dd2
Luděk Veselý <ludek.vesely@email.com>
2018-11-26 23:27:37 +0100

    create order form

---
+++ src/Controller/OrderForm.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/order-form")
+ */
+class OrderForm extends AbstractController
+{
+    /**
+     * @Route("/", name="order_form_index")
+     */
+    public function index(): Response
+    {
+        return $this->render('order_form/index.html.twig');
+    }
+}
