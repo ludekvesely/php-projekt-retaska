7c1d378a5e10cd30e48d8681f58c0cded001377e
Luděk Veselý <ludek.vesely@email.com>
2018-11-28 23:45:26 +0100

    show categories on frontent

--- src/Controller/ProductListController.php
+++ src/Controller/ProductListController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,12 +15,30 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProductListController extends AbstractController
 {
     /**
+     * @Route("/{id}", name="product_list_category")
+     */
+    public function category(
+        ProductRepository $productRepository,
+        CategoryRepository $categoryRepository,
+        Category $category
+    ): Response {
+        $products = $productRepository->findBy(['category' => $category]);
+        $categories = $categoryRepository->findAll();
+
+        return $this->render('product_list/index.html.twig', [
+            'products' => $products,
+            'categories' => $categories,
+            'currentCategory' => $category
+        ]);
+    }
+
+    /**
      * @Route("/", name="product_list_index")
      */
-    public function index(ProductRepository $productRepository): Response
+    public function index(CategoryRepository $categoryRepository): Response
     {
-        $products = $productRepository->findAll();
+        $categoryId = $categoryRepository->findFirst()->getId();
 
-        return $this->render('product_list/index.html.twig', ['products' => $products]);
+        return $this->redirectToRoute('product_list_category', ['id' => $categoryId]);
     }
 }
