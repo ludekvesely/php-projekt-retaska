fc0a2c67ff2789673f71ff61e5e402267873f23f
Luděk Veselý <ludek.vesely@email.com>
2018-12-20 11:20:50 +0100

    add products search

--- src/Controller/ProductListController.php
+++ src/Controller/ProductListController.php
@@ -3,9 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use App\Entity\Product;
 use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,6 +17,28 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class ProductListController extends AbstractController
 {
+    /**
+     * @Route("/search", name="product_list_search")
+     */
+    public function search(
+        CategoryRepository $categoryRepository,
+        EntityManagerInterface $entityManager,
+        Request $request
+    ): Response {
+        $search = $request->query->get('search');
+
+        $products = $entityManager
+            ->createQuery("SELECT p FROM " . Product::class . " p WHERE p.name LIKE :search")
+            ->setParameter('search', "%$search%")
+            ->getResult();
+
+        return $this->render('product_list/index.html.twig', [
+            'products' => $products,
+            'categories' => $categoryRepository->findAll(),
+            'search' => $search
+        ]);
+    }
+
     /**
      * @Route("/{id}", name="product_list_category")
      */
